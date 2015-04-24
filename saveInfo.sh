fe27f2117e47760ba6b14626774dedb0939bc60f
Hack Reactor Students <accounts@hackreactor.com>
2015-04-24 16:43:08 -0700

    savePoint before concating

---
+++ saveInfo.sh
@@ -0,0 +1,5 @@
+--saveinfo:    Executing command account list
+data:    Name        Id                                    Current
+data:    ----------  ------------------------------------  -------
+data:    Free Trial  592f2a80-cd4a-49e0-8918-78f99afaf9e0  true
+info:    account list command OK
